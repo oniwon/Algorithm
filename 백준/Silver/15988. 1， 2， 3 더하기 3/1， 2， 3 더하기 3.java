b1df170b8223fbc24a7fc94c44ec775ff5a7d6ac
oniwon <134580213+oniwon@users.noreply.github.com>
2024-06-01 23:47:22 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 592 ms, Memory: 22096 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static final int MOD = 1000000009;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        long[] dp = new long[1000001];
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+            for (int j = 4; j <= n; j++) {
+                dp[j] = (dp[j - 3] + dp[j - 2] + dp[j - 1]) % MOD;
+            }
+            sb.append(dp[n]).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+}
