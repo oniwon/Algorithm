c970d9e51c392b9071edb5bb7877c45c41faded7
oniwon <cookie89800@naver.com>
2024-09-16 23:55:53 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 18796 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,51 +1,53 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()); // 입력 개수
+        StringBuilder sb = new StringBuilder();
 
         Stack<Integer> stack = new Stack<>();
+
         for(int i = 0; i < n; i++) {
-            String cmd = br.readLine();
-            StringTokenizer st = new StringTokenizer(cmd, " ");
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
 
-            if(st.nextToken().equals("push")) {
-                int x = Integer.parseInt(st.nextToken());
-                stack.push(x);
+            String cmd = st.nextToken();
+
+            // push X
+            if(cmd.equals("push")) {
+                int X = Integer.parseInt(st.nextToken());
+                stack.push(X);
             }
 
-            if(cmd.equals("pop")) {
-                if(!stack.isEmpty()) {
-                    System.out.println(stack.peek());
-                    stack.pop();
-                } else {
-                    System.out.println(-1);
-                }
-            }
-
-            if(cmd.equals("size")) {
-                System.out.println(stack.size());
-            }
-
-            if(cmd.equals("empty")) {
+            // pop
+            else if(cmd.equals("pop")) {
                 if(stack.isEmpty()) {
-                    System.out.println(1);
-                } else System.out.println(0);
+                    sb.append(-1).append("\n");
+                } else sb.append(stack.pop()).append("\n");
             }
 
-            if(cmd.equals("top")) {
+            // size
+            else if(cmd.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            }
+
+            // empty
+            else if(cmd.equals("empty")) {
                 if(stack.isEmpty()) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(stack.peek());
-                }
+                    sb.append(1).append("\n");
+                } else sb.append(0).append("\n");
             }
 
 
+            // top
+            else if(cmd.equals("top")) {
+                if(stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else sb.append(stack.peek()).append("\n");
+            }
         }
-
+        System.out.print(sb);
     }
 }
