ad1a8718092dc58749fd13c2ec22f0cffe491ee0
oniwon <cookie89800@naver.com>
2024-10-07 06:06:01 +0900

    [Silver III] Title: 2×n 타일링, Time: 100 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -5,15 +5,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[n + 1]; //n 까지의 값을 저장하기 위한 배열
+        int[] dp = new int[n + 1]; // dp 배열
 
-        //초기값
-//        dp[1] = 1;
-//        dp[2] = 2; // n=1일때 ArrayIndexOutOfBoundsException => 1) 던지거나 2) 배열을 new int[1001] 로 설정하거나
-        dp[0] = dp[1] = 1;
+        // 초기화
+        dp[0] = 1;
+        dp[1] = 1;
+
         for(int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
-        System.out.println(dp[n]);
+        System.out.println(dp[n]); //  2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지
     }
 }
