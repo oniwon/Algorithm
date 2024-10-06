171aef3ec4e36f339c73272af1c76488dfd4167d
oniwon <cookie89800@naver.com>
2024-10-07 05:40:51 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -7,13 +7,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[n + 1]; //n 까지의 값을 저장하기 위한 배열
+        int[] dp = new int[n + 1]; // dp[i] 값(최소 연산 횟수)을 저장하기 위한 배열
 
-        dp[1] = 0; //초기값
+        dp[1] = 0; // 1은 이미 1이므로 0으로 초기화
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + 1; //-1 가능할 경우 dp[i] 값 갱신
-            if(i%2 == 0) dp[i] = min(dp[i], dp[i/2] + 1); //2로 나누기 가능할 경우
-            if(i%3 == 0) dp[i] = min(dp[i], dp[i/3] + 1); //3으로 나누기 가능할 경우
+            dp[i] = dp[i-1] + 1; // i-1 이 가능한 경우, 연산 횟수 1 추가
+            if(i%2 == 0) dp[i] = min(dp[i], dp[i/2] + 1); // 나누기 2가 가능한 경우, 이와 비교해서 최소 연산 횟수 선택
+            if(i%3 == 0) dp[i] = min(dp[i], dp[i/3] + 1); // 나누기 3이 가능한 경우, 이와 비교해서 최소 연산 횟수 선택
         }
         System.out.println(dp[n]);
     }
