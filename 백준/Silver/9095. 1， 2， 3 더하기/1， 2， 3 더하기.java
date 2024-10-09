fdd055e9444fc359b913f69e4548d215c28f9966
oniwon <cookie89800@naver.com>
2024-10-09 15:46:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,21 +4,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int t = Integer.parseInt(br.readLine());
-        int[] dp = new int[11];
-        for (int i = 0; i < t; i++) {
 
+        for(int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-//            int[] dp = new int[n + 1]; //동적 생성 => 런타임 에러
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-            for (int j = 4; j <= n; j++) {
-                dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
+            int[] dp = new int[11]; // n은 양수이며 11보다 작으므로
+
+            dp[1] = 1; // 1
+            dp[2] = 2; // 1+1, 2
+            dp[3] = 4; // 1+1+1, 1+2, 2+1, 3
+            for(int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3]; // dp[j - 1] 에서 1더하기 + dp[j - 2]에서 2더하기 + dp[j - 3]에서 3더하는 경우의 수
             }
             sb.append(dp[n]).append("\n");
-
         }
         System.out.println(sb);
     }
