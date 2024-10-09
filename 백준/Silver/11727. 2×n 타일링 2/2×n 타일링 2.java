8d031869f67769481855b7ac4aac1b0332b16ba4
oniwon <cookie89800@naver.com>
2024-10-09 15:02:15 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -5,12 +5,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[1001];
+        int[] dp = new int[n + 1]; // dp 배열
 
+        // 초기값
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 3;
-        for(int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007; // dp[i - 1]에서 1*2 타일 붙이기 + dp[i - 2]에서 1*2 두개, 2*2 타일 붙이기
         }
         System.out.println(dp[n]);
     }
