1e6db5a4fe983605229d11c3130622c1d66f71e7
oniwon <cookie89800@naver.com>
2024-10-13 22:13:00 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -3,15 +3,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        // dp[n] = N자리 이친수 개수
-        long dp[] = new long[n + 1];
-        
+        long[] dp = new long[n + 1]; // 이친수의 개수를 담을 배열
+
+        // 초기값
         dp[0] = 0;
         dp[1] = 1;
-
         for(int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
