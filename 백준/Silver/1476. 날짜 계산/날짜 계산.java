2e2ea9a233c03adddb40b4ad2cc118542c90d1e7
oniwon <cookie89800@naver.com>
2024-10-21 02:15:53 +0900

    [Silver V] Title: 날짜 계산, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.java
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,22 +12,23 @@ public class Main {
         int S = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        // 초기값
         int e = 1;
         int s = 1;
         int m = 1;
         int result = 1;
-        while (true) {
+
+        while (true) { // E, S, M을 찾을 때까지 반복
             if (e == E && s == S && m == M) break;
 
             e++;
-            if (e == 16) e = 1;
-
             s++;
-            if (s == 29) s = 1;
-
             m++;
-            if (m == 20) m = 1;
 
+            if (e == 16) e = 1; // E는 1부터 15까지 반복
+            if (s == 29) s = 1; // S는 1부터 28까지 반복
+            if (m == 20) m = 1; // M은 1부터 19까지 반복
+            
             result++;
         }
         System.out.println(result);
