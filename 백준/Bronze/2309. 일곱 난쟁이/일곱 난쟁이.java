b5e6b95b911943ebfbf09e239ba96a9a7ad9bad9
oniwon <cookie89800@naver.com>
2024-10-21 01:54:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
@@ -6,31 +8,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arr = new int[9];
+
         int sum = 0;
         for (int i = 0; i < 9; i++) {
-            int n = Integer.parseInt(br.readLine());
-            arr[i] = n;
-            sum += arr[i];
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i]; // 난쟁이 키 총합
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr); // 오름차순 정렬
 
         int num1 = 0, num2 = 0;
-        outer:
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 1; j < arr.length; j++) {
-                if (sum - arr[i] - arr[j] == 100) {
-                    num1 = arr[i]; // 뺄 값
-                    num2 = arr[j]; // 뺄 값
-                    break outer; // 두 값 찾으면 바로 빠져나가도록
+        for(int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if(sum - arr[i] - arr[j] == 100) { // 두 값을 빼서 합이 100인 두 숫자를 찾음
+                    num1 = arr[i];
+                    num2 = arr[j];
+                    break;
                 }
             }
         }
 
-        for (int i : arr) {
-            if(i == num1 || i == num2) continue;
-            System.out.println(i);
+        for (int i = 0; i < 9; i++) {
+            if (arr[i] != num1 && arr[i] != num2) { // 범인 제외
+                System.out.println(arr[i]);
+            }
         }
-
     }
 }
