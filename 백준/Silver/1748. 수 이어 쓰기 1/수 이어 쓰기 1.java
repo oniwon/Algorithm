e53a8bffd41c65a4cb51f8a5a9871c7d45d8e574
oniwon <cookie89800@naver.com>
2024-10-24 04:26:10 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,26 +1,22 @@
-import java.io.*;
-import static java.lang.Math.pow;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s = br.readLine();
-        int length = s.length(); // n 의 자릿수
-
         int n = Integer.parseInt(s);
 
-
-        double sum1 = 0, sum2 = 0;
-        for(int i = 1; i < length; i++) {
-            sum1 += i * (9 * pow(10, i-1));
+        double sum1 = 0;
+        for (int i = 1; i < s.length(); i++) {
+            sum1 += i * 9 * Math.pow(10, i - 1);
         }
 
-        sum2 = length * (n - pow(10, length-1) + 1); // 마지막 자릿수
+        double sum2 = 0;
+        sum2 = s.length() * (n - Math.pow(10, s.length() - 1) + 1); // 마지막 자리수 계산
 
-        int result = (int) (sum1 + sum2);
-        
-        System.out.println(result);
-        
+        System.out.println((int) (sum1 + sum2));
+
     }
 }
