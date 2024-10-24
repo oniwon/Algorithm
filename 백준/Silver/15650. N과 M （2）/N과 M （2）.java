039479d192214a1a92c78e2a9619edb02acfef18
oniwon <cookie89800@naver.com>
2024-10-24 12:13:30 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,14 +1,15 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static int n; // 1부터 n 까지
     public static int m; // 깊이
-    public static int arr[]; // 출력 담을 배열
-    public static boolean v[]; // 방문한 노드 체크할 배열
+    public static int[] arr; // 출력 담을 배열
+    public static boolean[] v; // 방문한 노드 체크할 배열
     public static StringBuilder sb = new StringBuilder();
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,11 +21,11 @@ public class Main {
         arr = new int[m];
         v = new boolean[n];
 
-        dfs(0,0);
+        dfs(0, 0);
         System.out.println(sb);
     }
 
-    private static void dfs(int s, int d) {
+    private static void dfs(int d, int s) {
         if(d == m) { // 재귀 탈출 조건
             for(int i : arr) {
                 sb.append(i).append(" ");
@@ -34,12 +35,11 @@ public class Main {
         }
 
         for(int i = s; i < n; i++) {
-            if(v[i]) continue; // true (이미 노드 방문했을시 넘어가기)
-            v[i] = true; // 방문한 노드 true 로 바꿔주기
-            arr[d] = i + 1; // 깊이마다 1씩 더해서 출력하도록
-            dfs(i + 1, d + 1); // 자식 노드 방문, s+1부터 arr 채워지도록
+            if(v[i]) continue; // 이미 방문한 노드는 넘어가기
+            v[i] = true; // 방문한 노드 true 로 설정
+            arr[d] = i + 1; // 깊이마다 1씩 더해서 arr 에 저장
+            dfs(d + 1, i + 1); // 자식 노드 방문, s+1부터 arr가 채워지도록
             v[i] = false; // 방문 상태 되돌리기
         }
-
     }
 }
