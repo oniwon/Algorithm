73d53c4f13c41490839cef79da522b171f9e73b4
oniwon <cookie89800@naver.com>
2024-10-25 12:03:06 +0900

    [Silver III] Title: N과 M (3), Time: 440 ms, Memory: 68524 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,28 +1,25 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-    public static int n; // 1부터 n 까지
-    public static int m; // 깊이
-    public static int arr[]; // 출력 담을 배열
+    public static int n;
+    public static int m;
+    public static int[] arr;
     public static StringBuilder sb = new StringBuilder();
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        arr = new int[m];
+        arr = new int[m]; // 출력 담을 배열
 
         dfs(0);
         System.out.println(sb);
     }
 
-    private static void dfs(int d) {
+    public static void dfs(int d) {
         if(d == m) { // 재귀 탈출 조건
             for(int i : arr) {
                 sb.append(i).append(" ");
@@ -32,7 +29,7 @@ public class Main {
         }
 
         for(int i = 0; i < n; i++) {
-            arr[d] = i + 1; // 깊이마다 1씩 더해서 출력하도록
+            arr[d] = i + 1; // 깊이마다 1씩 더해서 arr 에 저장
             dfs(d + 1); // 자식 노드 방문
         }
     }
