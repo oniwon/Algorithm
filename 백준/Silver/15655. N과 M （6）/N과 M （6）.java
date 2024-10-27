99aa04ef5b89a62534ea8b4842cf519111fe76d4
oniwon <cookie89800@naver.com>
2024-10-28 01:17:29 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,34 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static int n;
-    public static int m; // 깊이
-
-    public static int[] arr;
+    public static int m;
+    public static int[] arr; // 입력 저장할 배열
     public static int[] p; // 출력 담을 배열
-
-    public static boolean[] v; // 방문 노드 체크할 배열
-
+    public static boolean[] visited;
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[n];
         p = new int[m];
-        v = new boolean[n];
+        visited = new boolean[n];
 
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
@@ -36,21 +33,21 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static void dfs(int s, int d) {
-        if (d == m) {    // 깊이 같을 때 출력
-            for (int i : p) {
+    public static void dfs(int d, int s) {
+        if(d == m) {
+            for(int i : p) {
                 sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = s; i < n; i++) {
-            if(v[i]) continue;
-            v[i] = true;
-            p[d] = arr[i];
-            dfs(i + 1, d + 1);
-            v[i] = false;
+        for(int i = s; i < n; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            p[d] = arr[i]; // 현재 깊이 d에서 arr[i] 값 넣기
+            dfs(d + 1, i + 1); // 다음 깊이로 재귀 호출, i+1 인덱스부터 선택해사 오름차순 되도록
+            visited[i] = false;
         }
     }
 }
