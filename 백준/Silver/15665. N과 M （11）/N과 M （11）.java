a92f48201b2b22946794a69adacafc59fc122757
oniwon <cookie89800@naver.com>
2024-10-29 17:36:28 +0900

    [Silver II] Title: N과 M (11), Time: 360 ms, Memory: 34220 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[] arr; // 입력 저장할 배열
+    public static int[] p; // 출력 담을 배열
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        p = new int[m];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int d) {
+        if(d == m) {
+            for(int i : p) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = 0; // 이전 값 기억
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == prev) continue; // 이전과 같은 값이면 스킵
+            p[d] = arr[i]; // 현재 깊이 d에 arr[i] 값 넣기
+            prev = arr[i]; // 현재 값을 prev에 저장
+            dfs(d + 1); // 다음 깊이로 재귀 호출
+        }
+    }
+}
