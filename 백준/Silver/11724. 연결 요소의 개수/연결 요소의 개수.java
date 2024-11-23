0868de027be437b718e35c624e767ee2dfd71795
oniwon <cookie89800@naver.com>
2024-11-23 23:32:15 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 612 ms, Memory: 141980 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,51 +1,56 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static List<List<Integer>> list;
-    private static boolean[] visited;
+    static int n;
+    static int m;
+    static  List<List<Integer>> list; // 인접 리스트
+    static boolean[] visited;
+    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int n = Integer.parseInt(st.nextToken()); // 정점 개수
-        int m = Integer.parseInt(st.nextToken()); // 간선 개수
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         list = new ArrayList<>();
+        visited = new boolean[n + 1];
+        result = 0;
 
-        for (int i = 0; i <= n; i++) { // 리스트 초기화
+        for(int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
 
-        visited = new boolean[n + 1]; // 1부터 시작
-
-        for(int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list.get(a).add(b);
-            list.get(b).add(a);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list.get(u).add(v);
+            list.get(v).add(u);
         }
 
-        int count = 0;
         for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
-                count++; // 호출한 개수 = 연결 요소 개수
                 dfs(i);
+                result++;
             }
         }
-        System.out.println(count);
+
+        System.out.println(result);
     }
 
-    private static void dfs(int node) {
+    public static void dfs(int node) {
         visited[node] = true;
-
-        for(int i : list.get(node)) {
-            if(!visited[i]) dfs(i); // i 방문 안 했을때만 호출
+        for (int num : list.get(node)) {
+            if (!visited[num]) {
+                dfs(num);
+            }
         }
     }
 }
