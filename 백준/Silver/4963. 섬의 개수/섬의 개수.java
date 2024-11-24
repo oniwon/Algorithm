4d9de765f1b790094bb4b08cc994cd959db53484
oniwon <cookie89800@naver.com>
2024-11-24 18:05:17 +0900

    [Silver II] Title: 섬의 개수, Time: 140 ms, Memory: 15840 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int w, h;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0};
+    static int[] dc = {-1, 0, 1, 1, 1, 0, -1, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            w = Integer.parseInt(st.nextToken()); // 너비
+            h = Integer.parseInt(st.nextToken()); // 높이
+
+            if (w == 0 && h == 0) break;
+
+            map = new int[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int landCount = 0;
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
+                        dfs(i, j);
+                        landCount++;
+                    }
+                }
+            }
+            sb.append(landCount).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nr = x + dr[i];
+            int nc = y + dc[i];
+
+            if (nr >= 0 && nr < h && nc >= 0 && nc < w && !visited[nr][nc] && map[nr][nc] == 1) {
+               dfs(nr, nc);
+            }
+        }
+    }
+}
