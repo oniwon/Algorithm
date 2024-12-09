c61291fce9bee6391d7d31c28fe9bd9ee98c8b02
oniwon <cookie89800@naver.com>
2024-12-09 11:04:01 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1348 ms, Memory: 130704 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+
+        for(int i : list) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
