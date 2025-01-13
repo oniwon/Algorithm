1311a4fa0f83a5ebbb32e292edd87d1eeda7d6cd
oniwon <cookie89800@naver.com>
2025-01-13 16:33:04 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,20 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
 
         long[] dp = new long[101];
         dp[1] = 1;
         dp[2] = 1;
         dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
 
-        for(int j = 4; j < 101; j++) {
-            dp[j] = dp[j-2] + dp[j-3];
+        for(int j = 6; j < 101; j++) {
+            dp[j] = dp[j - 5] + dp[j - 1];
         }
 
         for(int i = 0; i < t; i++) {
